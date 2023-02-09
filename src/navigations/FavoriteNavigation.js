911aea847c46d66482934d25052c2d45c3d621b9
Eddy Huaylla <ehuaylla@attachmedia.com>
2023-02-08 21:40:23 -0500

    Navegación hacia atrás en favoritos

--- src/navigations/FavoriteNavigation.js
+++ src/navigations/FavoriteNavigation.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 import { createStackNavigator } from "@react-navigation/stack";
+
 import { Favorite } from "../screens/Favorite";
+import { Pokemon } from "../screens/Pokemon";
 
 const Stack = createStackNavigator();
 
@@ -9,6 +11,14 @@ export const FavoriteNavigation = () => {
 	return (
 		<Stack.Navigator>
 			<Stack.Screen name="Favorite" component={ Favorite } options = { { title : "Favoritos" } } />
+			<Stack.Screen
+				name="Pokemon"
+				component={ Pokemon }
+				options={ {
+					title: "",
+					headerTransparent: true,
+				} }
+			/>
 		</Stack.Navigator>
 	)
 }
